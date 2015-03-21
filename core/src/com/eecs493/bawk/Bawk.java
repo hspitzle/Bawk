07077f86e98d7e421e3986228a31c9f1b68e6f7e
hspitzle <hspitzle@umich.edu>
2015-03-20 20:36:25 -0400

    Bawk class started

--- core/src/com/eecs493/bawk/Bawk.java
+++ core/src/com/eecs493/bawk/Bawk.java
@@ -13,9 +13,30 @@ public class Bawk extends Sprite
     private Texture textureLeft;
     private Texture textureRight;
 
+    float rotation;
+
     public Bawk()
     {
 
     }
-    
+
+    @Override
+    public float getRotation()
+    {
+        return rotation;
+    }
+
+    @Override
+    public void rotate90(boolean clockwise)
+    {
+        if(clockwise)
+            rotation += 90;
+        else
+            rotation -= 90;
+
+        if(rotation >= 360) {
+            rotation = 0;
+        }
+        super.rotate90(clockwise);
+    }
 }
