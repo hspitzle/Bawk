b50782094cfad3efe9653241aaf8994a883846e4
hspitzle <hspitzle@umich.edu>
2015-03-20 20:54:39 -0400

    started Utility class

---
+++ core/src/com/eecs493/bawk/Utility.java
@@ -0,0 +1,32 @@
+package com.eecs493.bawk;
+
+import java.awt.Point;
+
+/**
+ * Created by Hayden on 3/20/2015.
+ */
+public class Utility
+{
+    public enum Color {RED, GREEN, YELLOW, BLUE};
+
+    private static Point gridLocation()
+    {
+        return new Point(4, 200);
+    }
+
+
+
+    public static Point getLocation(int row, int col)
+    {
+        if(row > 11 || col > 11)
+            return null;
+
+        final int x[] = {0, 35, 70, 105, 140, 188, 236, 284, 332, 367, 402, 437};
+        final int y[] = {437, 402, 367, 332, 284, 236, 188, 140, 105, 70, 35, 0};
+
+        Point loc = new Point();
+        loc.setLocation(x[col] + gridLocation().getX(), y[row] + gridLocation().getY());
+        return loc;
+    }
+
+}
