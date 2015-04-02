f3674b2c2befa0b24c350029002e6680980a289a
hspitzle <hspitzle@umich.edu>
2015-04-02 17:25:38 -0400

    New Laser class

---
+++ core/src/Laser.java
@@ -0,0 +1,34 @@
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+
+
+/**
+ * Created by Hayden on 4/2/2015.
+ */
+public class Laser extends Sprite {
+
+    private int direction;
+
+    public Laser(){ }
+
+    public enum Direction { DOWN, RIGHT, UP, LEFT};
+
+    public Laser(Color color, int rotation, int x, int y){
+        super(new Texture("laser_blue.png"));
+
+        if (color == Color.PURPLE)
+            setTexture(new Texture("laser_purple.png"));
+        else if(color == Color.GREEN)
+            setTexture(new Texture("laser_green.png"));
+        else if(color == Color.YELLOW)
+            setTexture(new Texture("laser_yellow.png"));
+
+        direction = (Direction) rotation/90;
+
+        setPosition(x, y);
+    }
+
+
+}
