8dca55ea94520776a22d0ea07d5050e135cb0dc8
frankkm <frankkm@umich.edu>
2015-04-07 22:07:24 -0400

    Added code so that every 3 eggs the time between egg generations decreases

--- core/src/com/eecs493/bawk/Options.java
+++ core/src/com/eecs493/bawk/Options.java
@@ -22,7 +22,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup;
 
-import javafx.scene.control.RadioButton;
+//import javafx.scene.control.RadioButton;
 
 //import java.awt.Rectangle;
 
