8d0242124b0ff35eb450372bc08f437d5476da97
hspitzle <hspitzle@umich.edu>
2015-04-07 21:11:28 -0400

    New input detection...SWIPE!

--- core/src/com/eecs493/bawk/Play.java
+++ core/src/com/eecs493/bawk/Play.java
@@ -14,6 +14,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 
@@ -53,6 +59,11 @@ public class Play implements Screen {
 
     private Sound laserSound;
     private Sound bawkSound;
+
+    private ImageButton fireButton;
+//    private Stage stage;
+
+    private final int movement = 48;
 
     public Play(BawkGame game_){
         game = game_;
@@ -97,7 +108,74 @@ public class Play implements Screen {
         lastEggTime = TimeUtils.millis();
         lastMovement = TimeUtils.millis();
 
+        if(game.swipe){
+            Gdx.input.setInputProcessor(new SimpleDirectionGestureDetector(new SimpleDirectionGestureDetector.DirectionListener() {
+
+                @Override
+                public void onUp() {
+                    bawk.moveUp(movement);
+                    checkBounds();
+                }
+
+                @Override
+                public void onRight() {
+                    bawk.moveRight(movement);
+                    checkBounds();
+                }
+
+                @Override
+                public void onLeft() {
+                    bawk.moveLeft(movement);
+                    checkBounds();
+                }
+
+                @Override
+                public void onDown() {
+                    bawk.moveDown(movement);
+                    checkBounds();
+                }
+
+                @Override
+                public void onTap(){
+                    fire();
+                }
+            }));
+        }
+
+//        Texture helpTextureUp = new Texture("fire.png");
+//        Texture helpTextureDown = new Texture("fire2.png");
+//        SpriteDrawable helpDrawableUp = new SpriteDrawable(new Sprite(helpTextureUp));
+//        SpriteDrawable helpDrawableDown = new SpriteDrawable(new Sprite(helpTextureDown));
+//        helpDrawableUp.setMinWidth(Gdx.graphics.getWidth()/4);
+//        helpDrawableUp.setMinHeight(Gdx.graphics.getWidth() / 4);
+//        helpDrawableDown.setMinWidth(Gdx.graphics.getWidth()/4);
+//        helpDrawableDown.setMinHeight(Gdx.graphics.getWidth()/4);
+//        fireButton = new ImageButton(helpDrawableUp, helpDrawableDown);
+//        //howToButton = new TextButton("Help!", skin); // Use the initialized skin
+//        fireButton.setPosition(Gdx.graphics.getWidth()/2 +5, Gdx.graphics.getHeight()/7 -Gdx.graphics.getWidth()/5);
+//        //howToButton.setWidth(2 * Gdx.graphics.getWidth()/3);
+//
+//        fireButton.addListener(new ChangeListener() {
+//            @Override
+//            public void changed (ChangeEvent event, Actor actor) {
+//                //backgroundImage = new Texture ("howtoscreen.png");
+//                // playButton.setTouchable(Touchable.disabled);
+//                // howToButton.setTouchable(Touchable.disabled);
+//                // settingsButton.setTouchable(Touchable.disabled);
+//                long myShotTimer = 220;
+//                if(TimeUtils.millis() - lastLaser > myShotTimer) {
+//                    fire();
+//                }
+//            }
+//        });
+
         spawnEgg();
+    }
+
+    private void fire(){
+        bawk.shoot();
+        laserSound.play();
+        lastLaser = TimeUtils.millis();
     }
 
     private void drawBatch()
@@ -128,7 +206,9 @@ public class Play implements Screen {
                 laser.draw(batch);
         }
 
-        font.draw(batch, "Score: "+String.valueOf(score), 150, 100);
+        font.draw(batch, "Score: "+String.valueOf(score), 150, 780);
+
+//        fireButton.draw(batch, 100f);
 
         batch.end();
     }
@@ -143,21 +223,11 @@ public class Play implements Screen {
 
         detectOverlaps();
 
-        long myShotTimer = 220;
-        if((Gdx.input.isTouched() || Gdx.input.isButtonPressed(Input.Keys.SPACE))
-                && TimeUtils.millis() - lastLaser > myShotTimer) {
-            bawk.shoot();
-            laserSound.play();
-            lastLaser = TimeUtils.millis();
-        }
-
         // check if we need to create a new commet
         if(TimeUtils.millis() - lastEggTime > eggTimer) {
             spawnEgg();
             lastEggTime = TimeUtils.millis();
         }
-
-
     }
 
     private void detectOverlaps()
@@ -244,42 +314,50 @@ public class Play implements Screen {
 
     private void updateBawkLocation()
     {
+        if(game.swipe)
+            return;
+
         long movementTimer = 400;
         if(TimeUtils.millis() - lastMovement < movementTimer)
             return;
 
         lastMovement = TimeUtils.millis();
 
+        tiltToMove();
+        checkBounds();
+    }
+
+    private void tiltToMove()
+    {
         float tiltX = Gdx.input.getAccelerometerX();
         float tiltY = Gdx.input.getAccelerometerY();
         float boundary = 1.2f;
-        int movement = 48;
 
         if(Math.abs(tiltX) > Math.abs(tiltY)) {
             if (tiltX > boundary) {
-                bawk.setX(bawk.getX() - movement);
-                bawk.leftRotate();
+                bawk.moveLeft(movement);
             } else if (tiltX < -1f * boundary) {
-                bawk.setX(bawk.getX() + movement);
-                bawk.rightRotate();
+                bawk.moveRight(movement);
             }
         }
         else {
             if (tiltY > boundary) {
-                bawk.setY(bawk.getY() - movement);
-                bawk.downRotate();
+                bawk.moveDown(movement);
             } else if (tiltY < -1f * boundary) {
-                bawk.setY(bawk.getY() + movement);
-                bawk.upRotate();
+                bawk.moveUp(movement);
             }
         }
+    }
 
-//        keep within x bounds
+    private void checkBounds()
+    {
+        // keep within x bounds
         if(bawk.getX() > game.getLocationX(7))
             bawk.setX(game.getLocationX(7));
         else if(bawk.getX() < game.getLocationX(4))
             bawk.setX(game.getLocationX(4));
 
+        // keep within y bounds
         if(bawk.getY() < game.getLocationY(7))
             bawk.setY(game.getLocationY(7));
         else if(bawk.getY() > game.getLocationY(4))
@@ -289,6 +367,7 @@ public class Play implements Screen {
     @Override
     public void hide() {
         // called when current screen changes from this to a different screen
+        fireButton.setTouchable(Touchable.disabled);
     }
 
     @Override
@@ -301,7 +380,9 @@ public class Play implements Screen {
     }
 
     @Override
-    public void resize(int width, int height) {}
+    public void resize(int width, int height) {
+        fireButton.setTouchable(Touchable.enabled);
+    }
 
     @Override
     public void pause() {}
---
+++ core/src/com/eecs493/bawk/SimpleDirectionGestureDetector.java
@@ -0,0 +1,57 @@
+package com.eecs493.bawk;
+
+import com.badlogic.gdx.input.GestureDetector;
+
+/**
+ * Created by Hayden on 4/7/2015.
+ */
+public class SimpleDirectionGestureDetector extends GestureDetector {
+
+    public interface DirectionListener {
+        void onLeft();
+
+        void onRight();
+
+        void onUp();
+
+        void onDown();
+
+        void onTap();
+    }
+
+    public SimpleDirectionGestureDetector(DirectionListener directionListener) {
+        super(new DirectionGestureListener(directionListener));
+    }
+
+    private static class DirectionGestureListener extends GestureAdapter{
+        DirectionListener directionListener;
+
+        public DirectionGestureListener(DirectionListener directionListener){
+            this.directionListener = directionListener;
+        }
+
+        @Override
+        public boolean fling(float velocityX, float velocityY, int button) {
+            if(Math.abs(velocityX)>Math.abs(velocityY)){
+                if(velocityX>0){
+                    directionListener.onRight();
+                }else{
+                    directionListener.onLeft();
+                }
+            }else{
+                if(velocityY>0){
+                    directionListener.onDown();
+                }else{
+                    directionListener.onUp();
+                }
+            }
+            return super.fling(velocityX, velocityY, button);
+        }
+
+        @Override
+        public boolean tap(float x, float y, int count, int button) {
+            directionListener.onTap();
+            return super.tap(x, y, count, button);
+        }
+    }
+}
